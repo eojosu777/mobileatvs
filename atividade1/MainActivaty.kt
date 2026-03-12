bf25b6c3c34f525bbb32dd56d4eedc2ef7b17b36
eojosu777 <jvfra@discente.ifpe.edu.br>
2026-03-12 10:23:32 -0300

    Update MainActivaty.kt

--- atividade1/MainActivaty.kt
+++ atividade1/MainActivaty.kt
@@ -1,77 +1,98 @@
 package com.app.meuprimeiroprojeto
 
 import android.os.Bundle
-import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.app.meuprimeiroprojeto.ui.theme.MeuPrimeiroProjetoTheme
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
-            MeuPrimeiroProjetoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MessageCard()
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+            Column(modifier = Modifier.padding(20.dp)) {
+
+                MessageCard(
+                    Message("IFPE", "Localizado em Palmares", R.drawable.ifpe)
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                MessageCard(
+                    Message("Cristiano Ronaldo", "Melhor da História", R.drawable.cristiano)
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                MessageCard(
+                    Message("Catende", "Terra da mulher da sombrinha!", R.drawable.catende)
+                )
             }
         }
     }
 }
-data class Message(val author: string)
+
+data class Message(
+    val author: String,
+    val body: String,
+    val image: Int
+)
+
 @Composable
-
 fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(8.dp)) {
 
-    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
-            painter = painterResource(R.drawable.ifpe),
-            contentDescription = "imagem ifpe",
+            painter = painterResource(msg.image),
+            contentDescription = "Imagem",
             modifier = Modifier
-
                 .size(40.dp)
-
                 .clip(CircleShape)
         )
-
 
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
             Text(text = msg.author)
+
+            Spacer(modifier = Modifier.height(4.dp))
+
             Text(text = msg.body)
         }
     }
 }
 
-
 @Preview(showBackground = true)
 @Composable
+fun PreviewMessageCard() {
+    Column(modifier = Modifier.padding(20.dp)) {
 
-fun GreetingPreview() {
-    MeuPrimeiroProjetoTheme {
-        Greeting("Android")
+        MessageCard(
+            Message("IFPE", "Localizado em Palmares", R.drawable.ifpe)
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        MessageCard(
+            Message("Cristiano Ronaldo", "Melhor da História", R.drawable.cristiano)
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        MessageCard(
+            Message("Catende", "Terra da mulher da sombrinha!", R.drawable.catende)
+        )
     }
 }
